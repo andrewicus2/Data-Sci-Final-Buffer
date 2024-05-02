6a971a2cc1568e28795837db4ab4475f7283fa16
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2024-05-02 19:09:29 +0200

    insights data

--- pages/05 🧑‍💻 Insights.py
+++ pages/05 🧑‍💻 Insights.py
@@ -37,7 +37,7 @@ most_responsive_range = average_response.idxmax()
 # Find the average rececny within the most responsive range
 most_responsive_recency_avg = df.loc[df['Recency_bins'] == most_responsive_range, 'Recency'].mean()
 
-st.metric(value = str(round(most_responsive_recency_avg, 2)) + "days", label = "Recency")
+st.metric(value = str(round(most_responsive_recency_avg, 2)), label = "Days since last visit")
 
 df['NumWebVisitsMonth_bins'] = pd.cut(df['NumWebVisitsMonth'], bins=num_bins)
 
@@ -47,4 +47,6 @@ most_responsive_range = average_response.idxmax()
 
 most_responsive_NumWebVisitsMonth_avg = df.loc[df['NumWebVisitsMonth'] == most_responsive_range, 'NumWebVisitsMonth'].mean()
 
-st.metric(value = str(round(most_responsive_NumWebVisitsMonth_avg, 2)), label = "Visits per Month")
+st.metric(value = str(round(most_responsive_NumWebVisitsMonth_avg, 2)), label = "Web visits per month")
+
+
