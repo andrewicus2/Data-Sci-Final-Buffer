49ca085c8a49f01bb6b4842a036f65b8e2fde8c5
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2024-05-02 02:42:10 +0200

    t

--- pages/02 🤖 Model Prediction.py
+++ pages/02 🤖 Model Prediction.py
@@ -13,6 +13,8 @@ from sklearn.metrics import classification_report
 from codecarbon import EmissionsTracker
 from sklearn.metrics import accuracy_score
 import time
+from shapash.explainer.smart_explainer import SmartExplainer
+
 
 url = "https://upload.wikimedia.org/wikipedia/commons/6/6a/DoorDash_Logo.svg"
 st.image(url,  output_format="PNG", width=300)
@@ -53,7 +55,8 @@ else:
     else:
         clf = DecisionTreeClassifier(max_depth=3)
         clf = clf.fit(X_train,y_train)
-        model_accuracy = clf.predict(X_test)
+        y_pred = clf.predict(X_test)
+        model_accuracy = metrics.accuracy_score(y_test, y_pred))
 
         import graphviz
         from sklearn.tree import export_graphviz
@@ -72,6 +75,14 @@ else:
         # Display the graph using streamlit_graphviz
         st.graphviz_chart(dot_data)
 
+        xpl = SmartExplainer(clf)
+
+        y_pred = pd.Series(y_pred)
+        X_test = X_test.reset_index(drop=True)
+        xpl.compile(x=X_test, y_pred=y_pred)
+
+        xpl.plot.features_importance()
+
     model_end_time = time.time()
     model_execution_time = model_end_time - model_start_time
 
@@ -79,8 +90,6 @@ else:
     emissions = tracker.stop()
 
     # Display the emissions and energy consumed
-
-    st.write(f"Estimated emissions for training the model: {emissions:.4f} kg of CO2")
 
     st.metric(label = "Accuracy", value = str(round(metrics.accuracy_score(y_test, model_accuracy)*100, 2)) + "%")
     st.metric(label = "Execution time:", value = str(round(model_execution_time, 2)) + "s")
--- pages/03 🧑‍💻 Explainable AI.py
+++ pages/03 🧑‍💻 Explainable AI.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+from shapash.explainer.smart_explainer import SmartExplainer
 
 url = "https://upload.wikimedia.org/wikipedia/commons/6/6a/DoorDash_Logo.svg"
 st.image(url,  output_format="PNG", width=300)
 
 st.title("Explainable AI")
+
+xpl = SmartExplainer(clf)
+
+y_pred = pd.Series(y_pred)
+X_test = X_test.reset_index(drop=True)
+xpl.compile(x=X_test, y_pred=y_pred)
 
 
