6a44e1468c4cdd8da794fcf310f8a58a0cfff182
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2024-05-02 19:03:24 +0200

    updated insights

--- pages/05 🧑‍💻 Insights.py
+++ pages/05 🧑‍💻 Insights.py
@@ -21,3 +21,21 @@ ideal_age = response_by_age.idxmax()
 highest_response_proportion = response_by_age.max()
 
 st.metric(value = ideal_age, label = "Ideal Birth Year")
+
+# Define the number of bins for recency segmentation
+num_bins = 10
+
+# Create bins for recency segmentation
+df['Recency_bins'] = pd.cut(df['Recency'], bins=num_bins)
+
+# Calculate the average response for each recency bin
+average_response = df.groupby('Recency_bins')['Response'].mean()
+
+# Find the recency range with the highest average response
+most_responsive_range = average_response.idxmax()
+
+# Find the average rececny within the most responsive range
+most_responsive_recency_avg = df.loc[df['Recency_bins'] == most_responsive_range, 'Recency'].mean()
+
+st.write("Most responsive recency range:", most_responsive_range)
+st.write("Average recency within the most responsive range:", most_responsive_recency_avg)
