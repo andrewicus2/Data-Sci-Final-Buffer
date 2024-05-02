288eeb313c7680aaa5c0aae4f07de8b33437ee0f
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2024-05-02 16:48:59 +0200

    mlflow images

--- pages/03 🧑‍💻 Explainable AI.py
+++ pages/03 🧑‍💻 Explainable AI.py
@@ -1,12 +1,12 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from PIL import Image
-from shapash.explainer.smart_explainer import SmartExplainer
 
 url = "https://upload.wikimedia.org/wikipedia/commons/6/6a/DoorDash_Logo.svg"
-st.image(url,  output_format="PNG", width=300)
+st.image(url, output_format="PNG", width=300)
 
 st.title("Explainable AI")
-
-
 
