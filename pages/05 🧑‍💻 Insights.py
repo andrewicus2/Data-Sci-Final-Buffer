32209e99554377bb62ae713d579231ffc61e481b
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2024-05-03 11:58:36 +0200

    Updated Insights

--- pages/05 🧑‍💻 Insights.py
+++ pages/05 🧑‍💻 Insights.py
@@ -78,3 +78,24 @@ most_responsive_range = average_response.idxmax()
 most_responsive_NumCatalogPurchases_avg = df.loc[df['NumCatalogPurchases'] == most_responsive_range, 'NumCatalogPurchases'].mean()
 
 st.metric(value = str(round(most_responsive_NumCatalogPurchases_avg, 2)), label = "Num of catalog purchases")
+
+
+st.write("""
+The most important variable is recency, symbolizing Doordash’s success in customer loyalty. It suggests that once a customer makes a purchase using the app, they will likely engage in our marketing campaigns and make repeated purchases. By targeting customers who recently made a purchase in our sixth campaign, we can continue to profit from their repeated purchasing behavior. These customers are more likely to be willing to pay for the DoorDash button, as they are likely to experience high satisfaction from the added convenience of ordering food. For the future, we will explore ways of how to expand our loyal customer base. 
+""")
+
+st.write("""
+The top 2, 3, and 4 variables pertain to whether the client accepted CMP5, CMP 3, and CMP 1. Therefore, for our sixth marketing campaign, we will adopt the same marketing strategies employed in these campaigns to achieve a high retention rate with our customer base. We will also examine the shortcomings of CMP2 and CMP 4, which did not yield a high response rate, to ensure that we avoid the same mistake in our sixth marketing campaign. 
+""")
+
+st.write("""
+The top 5 and 6 variables are linked to customer spending on food consumption; MntWines and MntMeatProducts. Customers that spent a considerable amount on wine and meat products in the last two years are likely to derive high satisfaction from the Doordash button, as it provides added convenience in obtaining these products. Moreover, given that wine is often considered a luxury rather than a necessity, we can infer that customers who have purchased a significant amount of wine are more inclined to invest in food experiences and thus, will likely invest in the button.
+""")
+
+st.write("""
+Information on the customer’s family, including marital status and whether they had a child or teenager at home, has a negligible effect on whether the customer responded to the marketing campaign. Unless this changes in the future, we do not need to expend resources gathering or analyzing this information. 
+""")
+
+st.write("""
+While this dataset gives us good insight into the correlation between whether the customer responded to past marketing campaigns and how they respond to the current one, it provides no information about the content of each marketing campaign. As a result, our analysis is entirely based on the recency of each campaign. This poses a significant limitation to our understanding of the data because the response rates to each campaign may have also been influenced by qualitative differences that we do not have access to. 
+""")
